14e10296dfc812f731d7a9ee0b287c48f5bc0289
roneetsingh <kaket786@gmail.com>
2021-07-16 22:54:18 +0530

    Environment Variables

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY ='5791628bb0b13ce0c676dfde280ba245'
+    SECRET_KEY =os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
