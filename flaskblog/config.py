f3243856aea09feafc78af5c1c954b0b7ca30519
roneetsingh <kaket786@gmail.com>
2021-07-16 19:17:50 +0530

    Dev Build

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     SECRET_KEY ='5791628bb0b13ce0c676dfde280ba245'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
